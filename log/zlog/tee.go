2688ab69fab92943934f172e38f150d1001d509e
ahang <ch1447571882@qq.com>
2024-06-29 01:08:30 +0800

    feat(log): add zlog based on zap

---
+++ log/zlog/tee.go
@@ -0,0 +1,32 @@
+package zlog
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"io"
+)
+
+type LevelEnablerFunc func(level Level) bool
+
+type TeeOption struct {
+	Out io.Writer
+	LevelEnablerFunc
+}
+
+func NewTee(tees []TeeOption, opts ...Option) *Logger {
+	var cores []zapcore.Core
+	for _, tee := range tees {
+		cfg := zap.NewProductionEncoderConfig()
+		cfg.EncodeTime = zapcore.RFC3339TimeEncoder
+		core := zapcore.NewCore(
+			zapcore.NewJSONEncoder(cfg),
+			zapcore.AddSync(tee.Out),
+			zap.LevelEnablerFunc(tee.LevelEnablerFunc),
+		)
+		cores = append(cores, core)
+	}
+	return &Logger{
+		l:  zap.New(zapcore.NewTee(cores...), opts...),
+		al: nil,
+	}
+}
